53b3c424e4ab6bc974875256786b66849f9a484b
Shikha Mishra <shikham@adobe.com>
2024-05-15 15:20:07 +0530

    auth flow

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '803869636455-muh3t01jnu5io0f7aquo6q71h62a278g.apps.googleusercontent.com',
+    googleClientSecret: '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',
+    mongoURI: 'mongodb+srv://shinyshikha11:KLvoeD858YG3xQev@cluster0.3sqob14.mongodb.net/emailydb?retryWrites=true&w=majority&appName=Cluster0',
+    cookieKey: 'iiisjdnscdjndjdddsacds'
+}
